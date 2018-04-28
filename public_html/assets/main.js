89bc64b45802dabff292675305bf511096242c8f
CHRISTIAN GUILHERME FRITSCH <201310383@academico.unilasalle.edu.br>
2018-04-28 10:44:11 -0300

    Correções 1

--- public_html/assets/main.js
+++ public_html/assets/main.js
@@ -4,4 +4,16 @@
  * and open the template in the editor.
  */
 
+$(document).ready(function() {
+  var $keywords = $('#keywords');
+  if ($keywords) {
+    $keywords.on('input', function(e) {
+      var words = $keywords.val().split(' ');
+      if (words.length > 5) {
+        $keywords.val(words.slice(0, 5).join(' '));
+        return;
+      }
+    });
+  }
+});
 
